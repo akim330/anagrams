81817ef985bca3f3f3393fb45db402283c771e10
Andrew Kim <akim330@gmail.com>
2020-05-30 13:55:59 -0400

    yo

--- anagrams_network3.py
+++ anagrams_network3.py
@@ -216,6 +216,7 @@ class banana(object):
         data = str(self.net.id) + "|" + str(self.seed) + "|" + str(self.current) + "|" + str(self.playerwords) + "|" + str(self.playerwords_list) + "|" + str(self.player2words) + "|" + str(self.player2words_list) + "|" + str(self.last_update) + "|" + str(self.used_tiles)
         print(f"DATA TO SEND: {data}")
         reply = self.net.send(data)
+        print(f"DATA RECEIVED: {reply}")
 
         if time_check:
             end_time = time.time()
