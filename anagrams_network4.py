757963a009ef9643707049b4722a34ef37ffc9c9
Andrew Kim <akim330@gmail.com>
2020-05-31 02:04:32 -0400

    y

--- anagrams_network4.py
+++ anagrams_network4.py
@@ -708,7 +708,7 @@ class banana(object):
             print(f"Tiles used from opponent: {used_tiles_recv}")
             print(f"Tiles used by me: {self.used_tiles}")
             # print(f"take_start_time: {self.take_start_time}, player 2 last update: {self.player2_last_update}, take end time: {self.take_end_time}, current: {self.current}, used_tiles_recv: {used_tiles_recv}")
-            if not ((self.take_start_time < self.player2_last_update < self.take_end_time + datetime.timedelta(0,0.5)) and any(x in self.used_tiles for x in used_tiles_recv)):
+            if not ((self.take_start_time < self.player2_last_update < self.take_end_time + datetime.timedelta(0,1)) and any(x in self.used_tiles for x in used_tiles_recv)):
                 print("UPDATING")
                 if self.player2words_list == player2words_list_recv and self.playerwords_list == playerwords_list_recv:
                     # print("JUST A FLIP")
