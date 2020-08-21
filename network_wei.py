080136d0e750cb8f17fbc219c0e45ff3442fc505
Andrew Kim <akim330@gmail.com>
2020-08-20 20:17:42 -0400

    up

--- network_wei.py
+++ network_wei.py
@@ -4,7 +4,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "65.96.225.231" # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
+        self.host = "25.17.18.143" # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
                                     # You can find this address by typing ipconfig in CMD and copying the ipv4 address. Again this must be the servers
                                     # ipv4 address. This feild will be the same for all your clients.
         self.port = 5555
