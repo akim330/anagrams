e8146c5388b87e8d0f69bc876684e66f2efd6819
Andrew Kim <akim330@gmail.com>
2020-08-25 13:43:59 -0400

    message

--- network.py
+++ network.py
@@ -31,7 +31,7 @@ class Network:
         """
         #try:
         self.client.send(pickle.dumps(data))
-        return pickle.loads(self.client.recv(8192))
+        return pickle.loads(self.client.recv(32768))
         """
         except:
             print("Error!")
